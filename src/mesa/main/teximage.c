0fca2737208048cd30f53b5022311d013793ebce
Eric Engestrom <eric.engestrom@imgtec.com>
2018-02-28 16:02:17 +0000

    mesa: use `sizeof name` instead of a hard-coded length
    Signed-off-by: Eric Engestrom <eric.engestrom@imgtec.com>

--- src/mesa/main/teximage.c
+++ src/mesa/main/teximage.c
@@ -1910,7 +1910,7 @@ texture_error_check( struct gl_context *ctx,
     * requires GL_OES_texture_float) are filtered elsewhere.
     */
    char bufCallerName[20];
-   snprintf(bufCallerName, 20, "glTexImage%dD", dimensions);
+   snprintf(bufCallerName, sizeof bufCallerName, "glTexImage%dD", dimensions);
    if (_mesa_is_gles(ctx) &&
        texture_format_error_check_gles(ctx, format, type,
                                        internalFormat, bufCallerName)) {
